9a002b8c4ea207003068c6c0eaf55fd51e6f24f4
Chris Beltrán <chrisgonza0@gmail.com>
2020-04-18 14:32:05 -0600

    Add: ServicioProfesional Class

---
+++ Parcial01/src/com/ZAOF/x00355519/ServicioProfesional.java
@@ -0,0 +1,13 @@
+package com.ZAOF.x00355519;
+
+public class ServicioProfesional extends Empleado{
+    private int mesesContrato;
+
+    public int getMesesContrato() {
+        return mesesContrato;
+    }
+
+    public void setMesesContrato(int mesesContrato) {
+        this.mesesContrato = mesesContrato;
+    }
+}
