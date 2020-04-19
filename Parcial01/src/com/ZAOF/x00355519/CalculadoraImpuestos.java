6bfc7f41c4804fcab653d751e1e31c49953abb67
Chris Beltrán <chrisgonza0@gmail.com>
2020-04-19 14:57:16 -0600

    Add: MostrarTotales created

--- Parcial01/src/com/ZAOF/x00355519/CalculadoraImpuestos.java
+++ Parcial01/src/com/ZAOF/x00355519/CalculadoraImpuestos.java
@@ -1,66 +1,80 @@
 package com.ZAOF.x00355519;
 
-import java.util.Scanner;
-
 public class CalculadoraImpuestos {
-        private double TotalRenta;
-        private double TotalIsss;
-        private double TotalAFP;
+        private static double TotalRenta;
+        private static double TotalIsss;
+        private static double TotalAFP;
 
     private CalculadoraImpuestos() {
     }
 
-    public static void calcularPago(Empleado empleado) {
-        Scanner sca = new Scanner(System.in);
+    public static double calcularPago(Empleado empleado) {
+        double AFP = 0; double ISSS = 0;
+        double Renta = 0; double LiquidSalary = 0;
+
         if (empleado instanceof PlazaFija) {
-            double BaseSalary = Empleado.getSalario();
+            double BaseSalary = empleado.getSalario();
             double SalaryWithoutTaxes;
 
             //AFP
-            double AFP = (0.0625 * BaseSalary);
+            AFP = (0.0625 * BaseSalary);
+            TotalAFP += AFP;
 
             //ISSS
-            double ISSS = (0.03 * BaseSalary);
+            ISSS = (0.03 * BaseSalary);
+            TotalIsss += ISSS;
 
             SalaryWithoutTaxes = (BaseSalary - AFP - ISSS);
 
             //Renta
             if (SalaryWithoutTaxes > 0.01 && SalaryWithoutTaxes <= 472.00) {
-                double Renta = 0;
+                Renta = 0;
                 System.out.println("No debe pagar renta\n");
                 System.out.println("Salario a pagar al empleado: $" + (SalaryWithoutTaxes - Renta) +
                         " dolares");
+                TotalRenta += Renta;
             }
 
             if (SalaryWithoutTaxes > 472.00 && SalaryWithoutTaxes <= 895.24) {
-                double Renta1 = ((0.1 * (SalaryWithoutTaxes - 472)) + 17.67);
-                System.out.println("Debe pagar: $" + Renta1 + " dolares\n");
-                System.out.println("Salario a pagar al empleado: $" + (SalaryWithoutTaxes - Renta1) +
+                Renta = ((0.1 * (SalaryWithoutTaxes - 472)) + 17.67);
+                System.out.println("Debe pagar: $" + Renta + " dolares\n");
+                System.out.println("Salario a pagar al empleado: $" + (SalaryWithoutTaxes - Renta) +
                         " dolares");
+                TotalRenta += Renta;
             }
 
             if (SalaryWithoutTaxes > 895.24 && SalaryWithoutTaxes <= 2038.10) {
-                double Renta2 = ((0.2 * (SalaryWithoutTaxes - 895.25)) + 60);
-                System.out.println("Debe pagar: $" + Renta2 + " dolares\n");
-                System.out.println("Salario a pagar al empleado: $" + (SalaryWithoutTaxes - Renta2) +
+                Renta = ((0.2 * (SalaryWithoutTaxes - 895.25)) + 60);
+                System.out.println("Debe pagar: $" + Renta + " dolares\n");
+                System.out.println("Salario a pagar al empleado: $" + (SalaryWithoutTaxes - Renta) +
                         " dolares");
+                TotalRenta += Renta;
             }
 
             if (SalaryWithoutTaxes > 2038.10) {
-                double Renta3 = ((0.3 * (SalaryWithoutTaxes - 2038.10)) + 288.57);
-                System.out.println("Debe pagar: $" + Renta3 + " dolares\n");
-                System.out.println("Salario a pagar al empleado: $" + (SalaryWithoutTaxes - Renta3) +
+                Renta = ((0.3 * (SalaryWithoutTaxes - 2038.10)) + 288.57);
+                System.out.println("Debe pagar: $" + Renta + " dolares\n");
+                System.out.println("Salario a pagar al empleado: $" + (SalaryWithoutTaxes - Renta) +
                         " dolares");
+                TotalRenta += Renta;
             }
         }
 
         if (empleado instanceof ServicioProfesional){
-            double BaseSalary = Empleado.getSalario();
+            double BaseSalary = empleado.getSalario();
             System.out.println("Se le descontara el 10% de su salario como valor de renta\n");
-                double Renta = (0.1 * BaseSalary);
-                double LiquidSalary = BaseSalary - Renta;
+                Renta = (0.1 * BaseSalary);
+                LiquidSalary = BaseSalary - Renta;
                 System.out.println("Su salario es: $" + LiquidSalary + " dolares");
+            TotalRenta += Renta;
         }
 
+        return LiquidSalary;
+    }
+
+    public static void MostrarTotales(){
+        System.out.println("Total de impuesto sobre AFP:\n$" + TotalAFP + " dolares\n"
+                            + "Total de impuesto sobre Isss:\n$" + TotalIsss + " dolares\n"
+                                + "Total de impuesto sobre Renta:\n$" + TotalRenta + " dolares\n");
     }
 }
