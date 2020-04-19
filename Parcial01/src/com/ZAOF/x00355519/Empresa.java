a8c347772fc0faffbb9d36e48134485006f4e987
alexander857 <00355519@uca.edu.sv>
2020-04-18 20:29:25 -0600

    Add: methods addEmpleado and quitEmpleado

--- Parcial01/src/com/ZAOF/x00355519/Empresa.java
+++ Parcial01/src/com/ZAOF/x00355519/Empresa.java
@@ -22,9 +22,16 @@ public class Empresa {
     //metodos de agregar y quitar empleado
 
     public void addEmpleado(Empleado e){
-
+        planilla.add(e); //se agrega un empleado a la planilla
     }
     public void quitEmpleado(String nombre){
-
+        //se elimina un empleado de la planilla
+        planilla.removeIf(s ->{
+            if(s.getNombre().equalsIgnoreCase(nombre)){
+                return true;
+            }
+            else
+                return false;
+        });
     }
 }
